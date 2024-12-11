a8729336a2f72d690d49b5eaa9d69de7c0b3ff11
LOHANNA BIETI VIEIRA <lohanna.vieira@cs.up.edu.br>
2024-12-11 11:30:15 -0300

    d

--- API/API/modelos/Imc.cs
+++ API/API/modelos/Imc.cs
@@ -2,13 +2,12 @@ using System;
 
 namespace API.modelos;
 
-public class Imc
+    public class Imc
 {
-    public string ImcId { get; set; } = Guid.NewGuid().ToString();
-    public double? Altura { get; set; }
-    public double? Peso { get; set; }
-    public string? classificação { get; set; }
-    public string AlunoId { get; set; }
-    public Aluno? Aluno { get; set; }
-    public DateTime CriadoEm { get; set; } = DateTime.Now;
+    public int Id { get; set; }
+    public float Peso { get; set; }
+    public float Altura { get; set; }
+    public float ValorImc { get; set; }
+    public int AlunoId { get; set; } 
+    public Aluno Aluno { get; set; } 
 }
